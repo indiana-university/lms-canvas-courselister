3cc73082cfce7545c2d5947014fec2a61b9cf07d
chmaurer <chmaurer@iu.edu>
2020-08-21 13:39:56 -0400

    LMSA-6905 Make properties files for the specific service areas (canvas, redis, database, etc)

--- src/main/java/edu/iu/uits/lms/courselist/config/ToolConfig.java
+++ src/main/java/edu/iu/uits/lms/courselist/config/ToolConfig.java
@@ -4,22 +4,12 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ConfigurationProperties(prefix = "courselist")
-@PropertySource(value = {"classpath:env.properties",
-      "classpath:default.properties",
-      "classpath:application.properties",
-      "${app.fullFilePath}/lms.properties",
-      "${app.fullFilePath}/protected.properties",
-      "${app.fullFilePath}/security.properties"}, ignoreResourceNotFound = true)
-
-
 @Getter
 @Setter
 public class ToolConfig {
-   private String canvasBaseUrl;
    private String startANewCourseUrl;
    private String version;
    private String env;

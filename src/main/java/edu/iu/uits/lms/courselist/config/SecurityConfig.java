54fbafa89a1fb0ada7fd73276fff48f8c9fda322
Damian Sobieralski <dsobiera@iu.edu>
2023-04-28 16:08:51 -0400

    LMSA-7723 - updated

--- src/main/java/edu/iu/uits/lms/courselist/config/SecurityConfig.java
+++ src/main/java/edu/iu/uits/lms/courselist/config/SecurityConfig.java
@@ -73,7 +73,7 @@ public class SecurityConfig {
                     .withObjectPostProcessor(new LmsFilterSecurityInterceptorObjectPostProcessor())
                     .and()
                     .headers()
-                    .contentSecurityPolicy("style-src 'self'; form-action 'self'; frame-ancestors 'self' https://*.instructure.com")
+                    .contentSecurityPolicy("form-action 'self'; frame-ancestors 'self' https://*.instructure.com")
                     .and()
                     .referrerPolicy(referrer -> referrer
                             .policy(ReferrerPolicyHeaderWriter.ReferrerPolicy.SAME_ORIGIN));
